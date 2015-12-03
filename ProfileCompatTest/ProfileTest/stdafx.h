35ed7ba2282a2f1c84b7af6bf2992fe01f8cf6bb
erasoni <erasoni@zoho.com>
2015-12-03 21:50:05 +0900

    remove local lang description

--- ProfileCompatTest/ProfileTest/stdafx.h
+++ ProfileCompatTest/ProfileTest/stdafx.h
@@ -1,17 +1,10 @@
-// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
-// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
-// を記述します。
-//
-
 #pragma once
 
-#ifndef _WIN32_WINNT		// Windows XP 以降のバージョンに固有の機能の使用を許可します。                   
-#define _WIN32_WINNT 0x0501	// これを Windows の他のバージョン向けに適切な値に変更してください。
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501
 #endif						
 
 #include <stdio.h>
 #include <tchar.h>
 #include <windows.h>
 
-
-// TODO: プログラムに必要な追加ヘッダーをここで参照してください。
