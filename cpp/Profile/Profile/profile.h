5646c50b6e62ae625970c520e5d3e89c0122041c
fjUnc <fjUnc@THEXP>
2018-02-22 01:42:01 +0900

    Modify: wstring to string (use utf8 internally)

--- cpp/Profile/Profile/profile.h
+++ cpp/Profile/Profile/profile.h
@@ -6,331 +6,362 @@
 #include <memory>
 #include <vector>
 #include <fstream>
-#include <codecvt>
+// #include <codecvt>
 
 #include <boost/algorithm/string/trim.hpp>
 
 namespace Ambiesoft {
-// https://stackoverflow.com/a/2072890
-static bool ends_with(std::wstring const & value, std::wstring const & ending)
-{
-    if (ending.size() > value.size()) return false;
-    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
-}
+	typedef void* HashIniHandle;
 
-static std::wstring wpTrim(const std::wstring& sin)
-{
-    if (sin.empty())
-        return sin;
+	// https://stackoverflow.com/a/2072890
+	static bool ends_with(std::string const & value, std::string const & ending)
+	{
+		if (ending.size() > value.size()) return false;
+		return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+	}
 
-    std::string sout = boost::algorithm::trim_copy(sin);
-    if (sout.empty())
-        return sout;
+	static std::string wpTrim(const std::string& sin)
+	{
+		if (sin.empty())
+			return sin;
 
-    if (sout[0] == L'\'')
-    {
-        if (ends_with(sout, L"\'"))
-        {
-            sout = sout.substr(1, sout.size() - 2);// Substring(1, sout.Length - 2);
-        }
-    }
-    else if (sout[0] == L'\"')
-    {
-        if (ends_with(sout, L"\""))
-        {
-            sout = sout.substr(1, sout.size() - 2);// Substring(1, sout.Length - 2);
-        }
-    }
-    return sout;
-}
-class HashIni
-{
-public:
-    typedef std::map<std::wstring, std::vector<std::wstring> > HashSection;
-    typedef std::map<std::wstring, std::shared_ptr<HashSection> > HashAll;
+		std::string sout = boost::algorithm::trim_copy(sin);
+		if (sout.empty())
+			return sout;
 
+		if (sout[0] == '\'')
+		{
+			if (ends_with(sout, "\'"))
+			{
+				sout = sout.substr(1, sout.size() - 2);// Substring(1, sout.Length - 2);
+			}
+		}
+		else if (sout[0] == '\"')
+		{
+			if (ends_with(sout, "\""))
+			{
+				sout = sout.substr(1, sout.size() - 2);// Substring(1, sout.Length - 2);
+			}
+		}
+		return sout;
+	}
 
-private:
-    HashAll hash_;
-    HashIni()
-    {
-    }
-public:
-
-    HashAll& Hash()
-    {
-        return hash_;
-    }
-
-    static HashIni* CreateEmptyInstanceForSpecialUse()
-    {
-        return new HashIni();
-    }
-};
-
-struct Profile
-{
-    static std::pair<std::wstring, std::wstring> splitLine(const std::wstring& line)
-    {
-        size_t pos = line.find(L'=', 0);
-        if (pos == std::wstring::npos)
-            return std::pair<std::wstring, std::wstring>(line, std::wstring());
-
-        std::wstring key = line.substr(0, pos);
-        std::wstring val = line.substr(pos + 1);
-        return std::pair<std::wstring, std::wstring>(key, val);
-    }
-
-    static std::wstring getSectionName(const std::wstring& line)
-    {
-        std::wstring ret;
-        if (line.empty() || line[0] != L'[')
-            return ret;
-
-        for (size_t i = 1; i <line.size(); ++i)
-        {
-            wchar_t c = line[i];
-            if (c == L']')
-                break;
-
-            ret += c;
-        }
-        return ret;
-    }
-
-    static HashIni* ReadAll(const std::string& file, bool throwexception = false)
-    {
-        HashIni* hi = HashIni::CreateEmptyInstanceForSpecialUse();
-        HashIni::HashAll& al = hi->Hash();
-
-        try
-        {
-            // TODO: implement mutex
-            //mutex = createmutex(inipath);
-            //waitmutex(mutex);
-
-            std::wifstream fs;
-            fs.open(file);
-            if(!fs)
-                throw std::exception();
-
-            static_assert(sizeof(wchar_t) == 2, "error.");//Linux is no ready
-            fs.imbue(std::locale(std::locale(""), new std::codecvt_utf8_utf16<wchar_t, 0x10ffff, std::consume_header>()));
-            if (fs)
-            {
-                std::wstring line;
-                std::shared_ptr<HashIni::HashSection> cursec = nullptr;
-
-                while (getline(fs, line))
-                {
-                    boost::algorithm::trim_left(line);
-                    if (line.empty() || line[0] == L'#')
-                        continue;
-
-                    if (line[0] == L'[')
-                    {
-                        std::wstring secname = getSectionName(line);
-                        cursec = al[secname];
-                        if (!cursec)
-                        {
-                            cursec = std::make_shared<HashIni::HashSection>();
-                            al[secname] = cursec;
-                        }
-                        continue;
-                    }
-                    else  // not a section line
-                    {
-                        if (!cursec)
-                            continue;
-
-                        std::pair<std::wstring, std::wstring> vals = splitLine(line);
-                        if (!vals.first.empty())
-                            vals.first = wpTrim(vals.first);
-
-                        if (!vals.second.empty())
-                            vals.second = wpTrim(vals.second);
-
-                        std::vector<std::wstring>& arent = (*cursec)[vals.first];
-                        //if (arent == null)
-                        //{
-                        //	arent = new ArrayList();
-                        //	cursec[vals[0]] = arent;
-                        //}
-
-                        arent.push_back(vals.second);
-                    }
-
-                }
-            }
-            return hi;
-        }
-        catch (std::exception& e)
-        {
-            if (throwexception)
-                throw e;
-        }
-        return hi;
-    }
-    static bool WriteAll(HashIni* hi, const std::string& inipath)
-    {
-        return WriteAll(hi, inipath, false);
-    }
-    static bool WriteAll(HashIni* hi, const std::string& inipath, bool throwexception){
-        if (hi == nullptr)
-            return false;
-
-        HashIni::HashAll& al = hi->Hash();
-
-        // Mutex mutex = null;
-        try
-        {
-            //mutex = createmutex(inipath);
-            //waitmutex(mutex);
-
-            std::wofstream ofs;
-            ofs.open(inipath);
-            if(!ofs)
-                throw std::exception();
-
-            ofs.imbue({ {}, new std::codecvt_utf8<wchar_t, 0x10FFFF, std::consume_header> });
+	class Profile;
+	class HashIni
+	{
+	public:
+		typedef std::map<std::string, std::vector<std::string> > HashSection;
+		typedef std::map<std::string, std::shared_ptr<HashSection> > HashAll;
 
 
-            {
+	private:
+		HashAll hash_;
+		HashIni()
+		{
+		}
+		HashAll & Hash()
+		{
+			return hash_;
+		}
+	
+	public:
+		static HashIni* CreateEmptyInstanceForSpecialUse()
+		{
+			return new HashIni();
+		}
 
-                //ArrayList arKeys = new ArrayList(al.Keys);
-                //arKeys.Sort();
-                for (HashIni::HashAll::iterator it = al.begin(); it != al.end(); ++it)
-                {
-                    std::wstring secname = it->first;
-                    std::shared_ptr<HashIni::HashSection> sec = it->second;
+		friend Profile;
+	};
 
-                    ofs << L"[";
-                    ofs << secname;
-                    ofs << L"]";
-                    ofs << std::endl;
+	class Profile
+	{
+	public:
+		static std::pair<std::string, std::string> splitLine(const std::string& line)
+		{
+			size_t pos = line.find('=', 0);
+			if (pos == std::string::npos)
+				return std::pair<std::string, std::string>(line, std::string());
 
-                    if (sec == nullptr)
-                        continue;
+			std::string key = line.substr(0, pos);
+			std::string val = line.substr(pos + 1);
+			return std::pair<std::string, std::string>(key, val);
+		}
 
-                    for (HashIni::HashSection::iterator its = sec->begin(); its != sec->end(); ++its)
-                    {
-                        std::wstring keyname = its->first;
-                        std::vector<std::wstring>& arent = its->second;
-                        for (std::wstring& val : arent)
-                        {
-                            if (!val.empty())
-                            {
-                                ofs << keyname;
-                                ofs << L"=";
-                                ofs << val;
-                                ofs << std::endl;
-                            }
-                        }
-                    }
-                    ofs << std::endl;
-                }
-            }
-        }
-        catch (std::exception& e)
-        {
-            if (throwexception)
-                throw e;
+		static std::string getSectionName(const std::string& line)
+		{
+			std::string ret;
+			if (line.empty() || line[0] != '[')
+				return ret;
 
-            return false;
-        }
-        //finally
-        //{
-        //	if (mutex != null)
-        //	{
-        //		mutex.ReleaseMutex();
-        //	}
-        //}
-        return true;
-    }
+			for (size_t i = 1; i < line.size(); ++i)
+			{
+				char c = line[i];
+				if (c == ']')
+					break;
 
-    static bool GetString(const std::wstring& app, const std::wstring& key, const std::wstring& def, std::wstring& ret, HashIni* hi)
-    {
-        ret = def;
-        if (hi == nullptr)
-            return false;
+				ret += c;
+			}
+			return ret;
+		}
 
-        if (app.empty())
-            return false;
+		static HashIniHandle ReadAll(const std::string& file, bool throwexception = false)
+		{
+			HashIni* hi = HashIni::CreateEmptyInstanceForSpecialUse();
+			HashIni::HashAll& al = hi->Hash();
 
-        if (key.empty())
-            return false;
+			try
+			{
+				// TODO: implement mutex
+				//mutex = createmutex(inipath);
+				//waitmutex(mutex);
 
-        std::shared_ptr<HashIni::HashSection> sec = hi->Hash()[app];
-        if (sec == nullptr)
-            return false;
+				std::ifstream ifs;
+				ifs.open(file);
+				if (!ifs)
+					throw std::exception();
 
-        std::vector<std::wstring>& arent = (*sec)[key];
+				// static_assert(sizeof(char) == 2, "error.");//Linux is no ready
+				// fs.imbue(std::locale(std::locale(""), new std::codecvt_utf8_utf16<char, 0x10ffff, std::consume_header>()));
+				// fs.imbue(std::locale(std::locale(""), new std::codecvt_utf8_utf16<char, 0x10ffff, std::consume_header>()));
+				ifs.imbue(std::locale(""));
 
-        if (arent.empty())
-            return false;
+				// skip UTF-8 BOM
+				{
+					char a, b, c;
+					a = ifs.get();
+					b = ifs.get();
+					c = ifs.get();
+					if (a != (char)0xEF || b != (char)0xBB || c != (char)0xBF)
+					{
+						ifs.seekg(0);
+					}
+				}
 
-        ret  = arent[0];
-        return true;
-    }
-    static bool WriteString(const wchar_t* app, const wchar_t* key, const wchar_t* val, HashIni* hi)
-    {
-        if (hi == nullptr)
-            return false;
+				if (ifs)
+				{
+					std::string line;
+					std::shared_ptr<HashIni::HashSection> cursec = nullptr;
 
-        if (app == nullptr)
-            return false;
+					while (getline(ifs, line))
+					{
+						boost::algorithm::trim_left(line);
+						if (line.empty() || line[0] == '#')
+							continue;
 
-        if (key == nullptr)
-            return false;
+						if (line[0] == '[')
+						{
+							std::string secname = getSectionName(line);
+							cursec = al[secname];
+							if (!cursec)
+							{
+								cursec = std::make_shared<HashIni::HashSection>();
+								al[secname] = cursec;
+							}
+							continue;
+						}
+						else  // not a section line
+						{
+							if (!cursec)
+								continue;
 
-        std::shared_ptr<HashIni::HashSection> sec = hi->Hash()[app];
-        if (!sec)
-        {
-            sec = std::make_shared<HashIni::HashSection>();
-            hi->Hash()[app] = sec;
-        }
+							std::pair<std::string, std::string> vals = splitLine(line);
+							if (!vals.first.empty())
+								vals.first = wpTrim(vals.first);
 
-        std::vector<std::wstring>& arent = (*sec)[key];
+							if (!vals.second.empty())
+								vals.second = wpTrim(vals.second);
 
-        if (arent.empty())
-        {
-            arent.push_back(val);
-        }
-        else
-        {
-            arent[0] = val;
-        }
-        return true;
-    }
+							std::vector<std::string>& arent = (*cursec)[vals.first];
+							//if (arent == null)
+							//{
+							//	arent = new ArrayList();
+							//	cursec[vals[0]] = arent;
+							//}
 
-    static bool GetInt(const std::wstring& app, const std::wstring& key, int def, int& ret, HashIni* hi)
-    {
-        ret = def;
+							arent.push_back(vals.second);
+						}
 
-        std::wstring sret;
-        if (!GetString(app, key, std::wstring(), sret, hi))
-            return false;
+					}
+				}
+				return hi;
+			}
+			catch (std::exception& e)
+			{
+				if (throwexception)
+					throw e;
+			}
+			return (HashIniHandle)hi;
+		}
+		static bool WriteAll(HashIniHandle hih, const std::string& inipath)
+		{
+			return WriteAll(hih, inipath, false);
+		}
+		static bool WriteAll(HashIniHandle hih, const std::string& inipath, bool throwexception) 
+		{
+			HashIni* hi = static_cast<HashIni*>(hih);
+			if (hi == nullptr)
+				return false;
 
-        if (sret.empty())
-            return false;
+			HashIni::HashAll& al = hi->Hash();
 
-        try
-        {
-            ret = std::stoi(sret);
-        }
-        catch (std::exception&)
-        {
-            return false;
-        }
+			// Mutex mutex = null;
+			try
+			{
+				//mutex = createmutex(inipath);
+				//waitmutex(mutex);
 
-        return true;
-    }
-    static bool WriteInt(const wchar_t* app, const wchar_t* key, int val, HashIni* hi)
-    {
-        std::wstring sval = std::to_wstring(val);
-        return WriteString(app, key, sval.c_str(), hi);
-    }
-};
+				std::ofstream ofs;
+				ofs.open(inipath);
+				if (!ofs)
+					throw std::exception();
+
+				// ofs.imbue({ {}, new std::codecvt_utf8<char, 0x10FFFF, std::consume_header> });
+				ofs.imbue(std::locale(""));
+
+
+
+				//ArrayList arKeys = new ArrayList(al.Keys);
+				//arKeys.Sort();
+				for (HashIni::HashAll::iterator it = al.begin(); it != al.end(); ++it)
+				{
+					std::string secname = it->first;
+					std::shared_ptr<HashIni::HashSection> sec = it->second;
+
+					ofs << "[";
+					ofs << secname;
+					ofs << "]";
+					ofs << std::endl;
+
+					if (sec == nullptr)
+						continue;
+
+					for (HashIni::HashSection::iterator its = sec->begin(); its != sec->end(); ++its)
+					{
+						std::string keyname = its->first;
+						std::vector<std::string>& arent = its->second;
+						for (std::string& val : arent)
+						{
+							if (!val.empty())
+							{
+								ofs << keyname;
+								ofs << "=";
+								ofs << val;
+								ofs << std::endl;
+							}
+						}
+					}
+					ofs << std::endl;
+				}
+
+			}
+			catch (std::exception& e)
+			{
+				if (throwexception)
+					throw e;
+
+				return false;
+			}
+			//finally
+			//{
+			//	if (mutex != null)
+			//	{
+			//		mutex.ReleaseMutex();
+			//	}
+			//}
+			return true;
+		}
+		static void FreeHandle(HashIniHandle hih)
+		{
+			HashIni* hi = static_cast<HashIni*>(hih);
+			delete hi;
+		}
+		static bool GetString(const std::string& app, const std::string& key, const std::string& def, std::string& ret, HashIniHandle hih)
+		{
+			HashIni* hi = static_cast<HashIni*>(hih);
+			ret = def;
+			if (hi == nullptr)
+				return false;
+
+			if (app.empty())
+				return false;
+
+			if (key.empty())
+				return false;
+
+			std::shared_ptr<HashIni::HashSection> sec = hi->Hash()[app];
+			if (sec == nullptr)
+				return false;
+
+			std::vector<std::string>& arent = (*sec)[key];
+
+			if (arent.empty())
+				return false;
+
+			ret = arent[0];
+			return true;
+		}
+		static bool WriteString(const char* app, const char* key, const char* val, HashIniHandle hih)
+		{
+			HashIni* hi = static_cast<HashIni*>(hih);
+
+			if (hi == nullptr)
+				return false;
+
+			if (app == nullptr)
+				return false;
+
+			if (key == nullptr)
+				return false;
+
+			std::shared_ptr<HashIni::HashSection> sec = hi->Hash()[app];
+			if (!sec)
+			{
+				sec = std::make_shared<HashIni::HashSection>();
+				hi->Hash()[app] = sec;
+			}
+
+			std::vector<std::string>& arent = (*sec)[key];
+
+			if (arent.empty())
+			{
+				arent.push_back(val);
+			}
+			else
+			{
+				arent[0] = val;
+			}
+			return true;
+		}
+
+		static bool GetInt(const std::string& app, const std::string& key, int def, int& ret, HashIniHandle hih)
+		{
+			HashIni* hi = static_cast<HashIni*>(hih);
+			ret = def;
+
+			std::string sret;
+			if (!GetString(app, key, std::string(), sret, hi))
+				return false;
+
+			if (sret.empty())
+				return false;
+
+			try
+			{
+				ret = std::stoi(sret);
+			}
+			catch (std::exception&)
+			{
+				return false;
+			}
+
+			return true;
+		}
+		static bool WriteInt(const char* app, const char* key, int val, HashIniHandle hih)
+		{
+			std::string sval = std::to_string(val);
+			return WriteString(app, key, sval.c_str(), hih);
+		}
+	};
 
 
 }
--- cpp/Profile/TestProfile/main.cpp
+++ cpp/Profile/TestProfile/main.cpp
@@ -7,51 +7,58 @@
 #endif
 
 #include "../Profile/profile.h"
-// #include "C:/T/Profile/Profile/profile.h"
+
 
 using namespace std;
 
 using namespace Ambiesoft;
 
-void testwrite()
+void testwrite(string filename)
 {
-    string filename = "TestProfile.ini";
-    HashIni* pIni = Profile::ReadAll(filename, false);
+    HashIniHandle ini = Profile::ReadAll(filename, false);
 
-	VERIFY(Profile::WriteInt(L"mysection", L"mykey", 12345, pIni));
-	VERIFY(Profile::WriteInt(L"mysection", L"mykey", 54321, pIni));
+	VERIFY(Profile::WriteInt("mysection", "mykey", 12345, ini));
+	VERIFY(Profile::WriteInt("mysection", "mykey", 54321, ini));
 
-	VERIFY(Profile::WriteInt(L"mysection", L"mykey2", 21, pIni));
+	VERIFY(Profile::WriteInt("mysection", "mykey2", 21, ini));
 
-	VERIFY(Profile::WriteInt(L"mysection2", L"mykey21", 212, pIni));
-	VERIFY(Profile::WriteString(L"mysection2", L"mykey22", L"あああ", pIni));
+	VERIFY(Profile::WriteInt("mysection2", "mykey21", 212, ini));
+	VERIFY(Profile::WriteString("mysection2", "mykey22", u8"あああ", ini));
 
-    VERIFY(Profile::WriteAll(pIni, filename));
+    VERIFY(Profile::WriteAll(ini, filename));
 	
-	delete pIni;
+	Profile::FreeHandle(ini);
 }
-void testread()
+void testread(string filename)
 {
-    string filename = "TestProfile.ini";
-    HashIni* pIni = Profile::ReadAll(filename, false);
+    HashIniHandle ini = Profile::ReadAll(filename, false);
 
 	int intval;
-	wstring sval;
-	Profile::GetInt(L"mysection", L"mykey", -1, intval, pIni);
+	string sval;
+	Profile::GetInt("mysection", "mykey", -1, intval, ini);
 	assert(intval == 54321);
-	Profile::GetInt(L"mysection", L"mykey2", -1, intval, pIni);
+	Profile::GetInt("mysection", "mykey2", -1, intval, ini);
 	assert(intval == 21);
-	Profile::GetInt(L"mysection2", L"mykey21", -1, intval, pIni);
+	Profile::GetInt("mysection2", "mykey21", -1, intval, ini);
 	assert(intval == 212);
 
-	Profile::GetString(L"mysection2", L"mykey22", wstring(), sval, pIni);
-	assert(sval == L"あああ");
+	Profile::GetString("mysection2", "mykey22", string(), sval, ini);
+	assert(sval == u8"あああ");
 
-	delete pIni;
+	Profile::FreeHandle(ini);
+}
+bool file_exists(string file)
+{
+	ifstream my_file(file);
+	return my_file.good();
 }
 int main()
 {
-	testwrite();
-	testread();
+	string filename = "TestProfile.ini";
+	if (file_exists(filename))
+		testread(filename);
+
+	testwrite(filename);
+	testread(filename);
     return 0;
 }
